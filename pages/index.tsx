20a23a3b438183ebd3de20d0c688fb9bead7c2f9
BhagyeshPro <bhagyeshpaunikar2001@gmail.com>
2022-06-30 01:59:47 +0530

    Dev

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,19 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Twitter 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <main>
+        {/* Sidebar */}
+
+        {/* Feed */}
+
+        {/* Widget */}
+      </main>
 
       <h1>Twitter Clone</h1>
     </div>
